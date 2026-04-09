9338474a4b36686ba9e8709c56d00e07a50e0aff
kimwookjin219 <heros21982@gmail.com>
2026-04-09 18:12:47 +0900

    day011종료

---
+++ track003_java/java2026/src/com/the703/basic007_ex/For2Ex009.java
@@ -0,0 +1,20 @@
+package com.the703.basic007_ex;
+
+public class For2Ex009 {
+	public static void main(String[] args) {
+		
+		//ver1
+		System.out.print('A'); System.out.println();
+
+	}
+}
+/*
+이중 for ,while ,do while
+
+A
+AB
+ABC
+ABCD
+...
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+*/
