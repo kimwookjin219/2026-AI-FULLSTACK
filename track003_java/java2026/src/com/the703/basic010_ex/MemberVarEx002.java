32eece0940e0610d3a68bb0f6b4e2b4653e99967
kimwookjin219 <heros21982@gmail.com>
2026-04-23 11:07:57 +0900

    day021

--- track003_java/java2026/src/com/the703/basic010_ex/MemberVarEx002.java
+++ track003_java/java2026/src/com/the703/basic010_ex/MemberVarEx002.java
@@ -1,11 +1,46 @@
 package com.the703.basic010_ex;
 
+class Student {
+    String name = "홍길동";        
+    int kor = 90;                  
+    int eng = 85;                 
+    static int studentCount = 0;    
+
+    int total = kor + eng;   
+
+    static int maxScore = 100;     
+
+    public Student() {
+        studentCount++;             
+    }
+
+    public int getTotalScore() {
+        return kor + eng;        
+    }
+
+    public static void showStudentCount() {
+        System.out.println("전체 학생 수: " + studentCount);  
+    }
+
+   public void showName() {
+         System.out.println(name);  
+   }
+
+    public void showInfo() {
+        System.out.println("이름: " + name);            
+        System.out.println("총점: " + getTotalScore());    
+    }
+}
+
 public class MemberVarEx002 {
 	public static void main(String[] args) {
-		
+        Student s1 = new Student();     
+        Student s2 = new Student();     
+
+        s1.showInfo();                  
+        Student.showStudentCount(); 
 
 	}
-
 }
 /*
 패키지명 : com.company.java010_ex
@@ -59,67 +94,5 @@ public class MemberVarEx002 {
         Student.showStudentCount();    
     }
 }
-
-
-
-연습문제3)  멤버변수
-패키지명 : com.company.java010_ex
-클래스명 :  MemberVarEx003
-//- 문제 1. 다음 코드에서 인스턴스변수, 클래스변수, 지역변수를 구분하시오.  ( 보관되는 영역도 추가 )
-//- 문제 2. 인스턴스메서드와 클래스메서드를 구분하시오.  
-//- 문제 3. 오류가 발생하는 이유를 설명하시오.
-//- 문제 4. runtime data area 위치영역 그림그리기
-//- 문제 5. 다음과 같이 출력되도록 코드를 작성하시오.
-:: 주인 이름: std-1
-총 음식량: 175
-전체 급식판 수: 1
-
-
-:: 주인 이름: std-2
-총 음식량: 175
-전체 급식판 수: 2
-
- 
-class LunchTray {
-    String owner;        
-    int rice = 90;               
-    int soup = 85;               
-
-    static int trayCount = 0;      
-
-    static int totalFood = rice + soup;
-
-    static int maxRice = 100;       
-
-    public int getFoodAmount() {
-        return rice + soup;         
-    }
-
-    public static void showTrayCount() {
-        System.out.println("전체 급식판 수: " + trayCount);   
-    }
-
-    public static void showOwner() { 
-       System.out.println(owner);
-    }
-
-    public void showTray() {
-        System.out.println("\n\n:: 주인 이름: " + owner);                
-        System.out.println("총 음식량: " + getFoodAmount());     
-    }
-}
-
-
-public class MemberVarEx003 {
-   public static void main(String[] args) {
-        LunchTray tray1 = new LunchTray();   
-        tray1.showTray();                    
-        LunchTray.showTrayCount();         
-
-        LunchTray tray2 = new LunchTray();   
-        tray2.showTray();                   
-        LunchTray.showTrayCount();         
-   }
-} 
 
 */
