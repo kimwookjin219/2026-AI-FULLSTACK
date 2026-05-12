055783a663a1d9da729f47e64f20d7f8969324c2
kimwookjin219 <heros21982@gmail.com>
2026-05-12 18:08:06 +0900

    day032

---
+++ track003_java/java2026/src/com/the703/basic018_ex/ThreadEx001.java
@@ -0,0 +1,40 @@
+package com.the703.basic018_ex;
+
+import javax.swing.JOptionPane;
+
+class QuestionCount extends Thread{
+
+	@Override public void run() { 
+		for(int i=10; i>0;i--) {
+			System.out.println(i);
+			try { Thread.sleep(1000); } 
+			catch (InterruptedException e) {  e.printStackTrace(); }			
+		}
+	}		
+}
+
+public class ThreadEx001 {
+	public static void main(String[] args) {
+		QuestionCount cnt = new QuestionCount(); cnt.start();
+		String answer = JOptionPane.showInputDialog("사과 알파벳을 입력하세요.");
+				
+		System.out.println(answer);
+//		if(answer.equals("apple")) { System.out.println("정답입니다!"); cnt.stop(); }
+//		else                       { System.out.println("정답이 아닙니다.."); cnt.stop(); }
+		System.out.println( answer.toLowerCase().equals("apple") ? "정답": "오답" ); cnt.stop();
+		//                         다 소문자로 변환
+		
+	}
+}
+/*
+연습문제1)  Threadapp
+패키지명 : com.the703.basic018
+클래스명 : ThreadEx001
+1.  QuestionCount  - 10부터 1까지 
+      카운트 1초에 10 ,
+                2초에 9, 
+                3초에  8.....
+
+2. 사과알파벳을 입력하세요.
+   사과를 입력을받으면 정답입니다 / 정답이 아닙니다.
+*/
