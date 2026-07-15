7edda259b46a2346a84ecd3a3f226d4cacb377a6
kimwookjin219 <heros21982@gmail.com>
2026-07-15 18:21:04 +0900

    day070

---
+++ track008_nodeReact/back/config/db.js
@@ -0,0 +1,10 @@
+const { connect } = require('pm2');
+
+require('dotenv').config();
+
+module.exports={
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    connectString: process.env.DB_CONNECT
+};
+// config/db.js
