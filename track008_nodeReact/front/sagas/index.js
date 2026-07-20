955cdc1e8c1dcbd7cd9d394a4191488737fe12f2
kimwookjin219 <heros21982@gmail.com>
2026-07-20 18:18:47 +0900

    day073

---
+++ track008_nodeReact/front/sagas/index.js
@@ -0,0 +1,17 @@
+/**
+ * sagas/index.js
+ * ------------------------------
+ * 루트 사가(rootSaga) 설정파일
+ * - 모든 개별 saga(userSaga 등)를 합쳐서 실행
+ * - Redux-Saga의 all, fork 를 사용해서 병렬로 실행
+ */
+
+import {all, fork} from 'redux-saga/effects'; // 여러 saga를 동시에 실행하기 위해 쓰는 함수
+import userSaga    from './user';
+
+export default function* rootSaga(){
+    yield all([
+        fork(userSaga), // userSaga를 병렬 실행
+        //fork(postSaga),  // 예시
+    ]);
+}
